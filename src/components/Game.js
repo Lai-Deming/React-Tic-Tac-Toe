d6e2b6a63fe1634dd553dd5797102f38fd993b6b
Lai Deming <laideming935@gmail.com>
2023-12-11 10:10:51 +0200

    complete

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,21 +3,19 @@ import Board from "./Board";
 
 /* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
 export default () => {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   const handlePlay = (nextSquares) => {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   };
 
   const jumpTo = (nextMove) => {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   };
 
   const moves = history.map((squares, move) => {
